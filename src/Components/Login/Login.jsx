a55f60ba42f4b1e75e3462fb398e05a8bdd27948
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-06-12 09:51:49 -0300

    Add Logo SVG component.

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './Login.scss'
+import Logo from '../Logo/Logo'
 
 const Login = () => {
   return (
-    <div className='container'>
+    <main className='container'>
         <header className="header">
+            <Logo />
             <h1 className="title">Bem-vindo de volta</h1>
+            <h2 className="subtitle">Entre com sua conta para acessar o painel.</h2>
         </header>
-    </div>
+        <section className="input">
+            
+        </section>
+    </main>
   )
 }
 
