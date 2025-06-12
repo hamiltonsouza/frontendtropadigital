66ace2b9e928ea00a5e1da5ad073fc21b3fcb107
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-06-12 08:47:51 -0300

    Use Login component on App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div>
-      
+      <Login />
     </div>
   );
 }
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Login.scss'
+
+const Login = () => {
+  return (
+    <div className='container'>
+        <header className="header">
+            <h1 className="title">Bem-vindo de volta</h1>
+        </header>
+    </div>
+  )
+}
+
+export default Login
