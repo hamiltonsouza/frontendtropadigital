58f765d677a6bb5db7811b194188a188e1ada71b
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-06-13 01:26:40 -0300

    Decompose Login content component

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import './Login.scss'
-import Logo from '../../Components/Common/Logo'
-import Title from '../../Components/Common/Title'
-import Subtitle from '../../Components/Common/Subtitle'
-import Form from './Form'
 import Image from './Image'
+import LoginContent from './LoginContent'
 
 const Login = () => {
   return (
     <main className='container'>
-        <header className="header">
-            <Logo />
-            <Title typeClass="title" message="Bem-vindo de volta" />
-            <Subtitle typeClass="subtitle" message="Entre com sua conta para acessar o painel." />
-        </header>
-        <Form />
-        <Image />
+      <LoginContent />
+      <Image />
     </main>
   )
 }
---
+++ src/Pages/Login/LoginContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './Form'
+import Header from './Header'
+
+const LoginContent = () => {
+  return (
+    <section className="content">
+        <Header />
+        <Form />
+    </section>
+  )
+}
+
+export default LoginContent
